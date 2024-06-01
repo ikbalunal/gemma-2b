e586bb4a421e1a59a2829c477af4ea92ac0defd3
ikbalunal <eikbalunal@gmail.com>
2024-06-01 11:47:05 +0300

    Run Streamlit Codes

--- RunStreamlit.py
+++ RunStreamlit.py
@@ -1,4 +1,9 @@
-import json
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename: RunStreamlit.py
+# @Author: ikbal
+# @Time: 6/1/2024 3:06 AM
+
 import subprocess
 
 subprocess.run(["streamlit", "run", "StreamlitUI.py"])
